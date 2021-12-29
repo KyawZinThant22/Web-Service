a232504fe665f6531012a05cfa48684d2ffa5cb1
KyawZinThant22 <kyawthantver2@gmail.com>
2021-12-29 13:48:33 +0630

    about-section

---
+++ project/js/app.js
@@ -0,0 +1,10 @@
+
+$(".navbar-toggler").click(function (){
+    let isClass = $(".navbar-collapse").hasClass('show');
+
+    if ( isClass){
+        $(".menu-icon").removeClass("fa-times").addClass("fa-bars");
+    }else {
+        $(".menu-icon").removeClass(".fa-bars").addClass("fa-times")
+    }
+})
